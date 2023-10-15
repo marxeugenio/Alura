a9bafd7a9b34e4c9a4d409002d193278067d5dc5
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2023-10-15 17:44:40 -0300

    Create Orientação_a_Objetos.py

---
+++ Orientação_a_Objetos.py
@@ -0,0 +1,16 @@
+class ContaCorrente:
+
+    def __init__(self, codigo):
+        self.codigo = codigo
+        self.saldo = 0
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def __str__(self):
+        return "[>>Codigo {} Saldo {}<<]".format(self.codigo, self.saldo)
+    
+
+conta_do_gui = ContaCorrente(15)
+conta_do_gui.deposita(500)
+print(conta_do_gui)
