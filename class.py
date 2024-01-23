816e1d29e1ec6047c5bc033ef2959e84a4029dea
Marxouo <78555292+marxeugenio@users.noreply.github.com>
2024-01-22 22:34:58 -0300

    Create class.py

---
+++ class.py
@@ -0,0 +1,20 @@
+class Musica:
+  nome = ""
+  artista = ""
+  duracao = int
+
+musica1 = Musica()
+musica1.nome = "Bohemian Rhapsody"
+musica1.artista = "Queen"
+muisica1.duracao = 355
+
+musica2 = Musica()
+musica2.nome = "Imagine"
+musica2.artissta = "John Lennon"
+musica2.duracao = 183
+
+musica3 = Musica()
+musica3.nome = "Shape of You"
+musica3.artista = "Ed Sheran"
+musica3.duracao = 234
+
